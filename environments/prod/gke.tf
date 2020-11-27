316e43b2f7ea63887600d87a6cfc69268235e3b7
inyong.jang <inyong.jang@samsung.com>
2020-11-27 14:06:19 +0900

    prod gke cluster 수정

--- environments/prod/gke.tf
+++ environments/prod/gke.tf
@@ -14,13 +14,11 @@ resource "google_container_cluster" "primary" {
 
   cluster_autoscaling {
     enabled = true
-    resource_limits = [
-      {
-        resource_type = cpu
-        minimum = 4
-        maximum = 12
-      }
-    ]
+    resource_limits {
+      resource_type = cpu
+      minimum = 4
+      maximum = 12
+    }
   }
 
   master_auth {
